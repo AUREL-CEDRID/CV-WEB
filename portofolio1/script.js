9864b642d3f367576f96d6b0190ec02f47532a77
AUREL CEDRIC <121832336+AUREL-CEDRID@users.noreply.github.com>
2024-11-24 23:22:41 +0300

    Add files via upload

---
+++ portofolio1/script.js
@@ -0,0 +1,12 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    alert(`Merci pour votre message, ${name} !`);
+    
+    document.getElementById("contactForm").reset();
+  });
+  
